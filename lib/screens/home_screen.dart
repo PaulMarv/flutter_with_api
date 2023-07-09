6757635ac67cc98470e32608d4353ba3e1a5c12c
PaulMarv <mpsalako@gmail.com>
2023-07-09 19:31:52 +0100

    swiper and a card done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+import 'package:store_api_flutter_course/consts/global_colors.dart';
+import 'package:store_api_flutter_course/widgets/appbar_icons.dart';
+import 'package:store_api_flutter_course/widgets/feeds_widget.dart';
+import 'package:store_api_flutter_course/widgets/sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,15 +14,104 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _textEditingController;
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: const Scaffold(
-        body: Center(child: Text("Welcome to this course")),
-      ),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Home'),
+            leading: AppBarIcons(
+              function: () {},
+              icon: IconlyBold.category,
+            ),
+            actions: [
+              AppBarIcons(function: () {}, icon: IconlyBold.user_3),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 18),
+                TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    filled: true,
+                    fillColor: Theme.of(context).cardColor,
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide:
+                            BorderSide(color: Theme.of(context).cardColor)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.secondary),
+                    ),
+                    suffixIcon: Icon(
+                      IconlyLight.search,
+                      color: lightIconsColor,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.25,
+                  child: Swiper(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return const SaleWidget();
+                    },
+                    pagination: const SwiperPagination(
+                      alignment: Alignment.bottomCenter,
+                      builder: DotSwiperPaginationBuilder(
+                        color: Colors.white,
+                        activeColor: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Latest Products",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const Spacer(),
+                      AppBarIcons(
+                          function: () {
+                          },
+                          icon: IconlyBold.arrow_right_2),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 160, child: Feedwidget(),)
+              ],
+            ),
+          )),
     );
   }
 }
